7ea949b9d88dc19d40d3fe5a4e2b21f2f1c331ff
anabeatrizrosario-sys <ana.beatriz.rosario@escola.pr.gov.br>
2026-08-13 08:47:46 -0300

    ok

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 1  //Botão curtidas
-document.addEventlistener
+document.addEventlistener("DOMContentLoaded",()=>{
+    const likeBtn = document.query.Selector(".left-actions .action-btn:firts-child");
+    if (!likeBtn) return;
+    const likeSvgv = likeBtn.querySelector("svg");   
+
+    //localiza o contador 
+
+    let texNode = Array
+})
+    
