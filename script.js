4e3ae966ca401f255ca0cb65a4ce16af63e3144b
anabeatrizrosario-sys <ana.beatriz.rosario@escola.pr.gov.br>
2026-08-20 08:47:22 -0300

    ok

--- script.js
+++ script.js
@@ -6,6 +6,34 @@ document.addEventlistener("DOMContentLoaded",()=>{
 
     //localiza o contador 
 
-    let texNode = Array
+
+    let texNode = Array.from(likeBtn.childNodes.)find(node)=> node.nodeType
+    === Node. TEXT NODE && node.textContent.trim()!== ** 
+);
+
+//zera o contador
+let cont = 0;
+
+//atualizar
+if(textNode){
+    textNoe. textContent = ´0´;
+}
+
+//coração
+function applyLikedStyle (){
+likeSvg.Style.fill = "#ef4444";
+likeSvg.Style.stroke ="#ef4444";
+LikeSvg.style.color = "#ef4444";
+}
+
+//efeito curtida
+likeSvg.style.trnsform = "scale(1.3)";
+SetTimeout(()=> (likeSvg.style.tranform));
+
+    
+}
+
 })
+
+
     
